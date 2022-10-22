5c8bfd1df7f029b4df24ff77b89a06ef965dc171
danilsklyar <100023622+danilsklyar@users.noreply.github.com>
2022-10-22 16:01:49 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$(function(){
+    $('.header-slider').slick({
+        dots:true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="./img/prev-arrow.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="./img/next-arrow.svg" alt=""></button>',
+        vertical: true,
+    });
+
+
+    $('.product__name').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        focusOnSelect: true,
+        asNavFor: '.product__content',
+        vertical: true,
+        prevArrow: '<button type="button" class="product-prev"><img src="./img/product-prev.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="product-next"><img src="./img/product-next.svg" alt=""></button>',
+      });
+      $('.product__content').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.product__name',
+        fade: true,
+        arrows: false,
+      });
+})
